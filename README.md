e3b93944981894af48ee4acef439122708d00d43
Bashua Mutiat <mutiatbashua@gmail.com>
2023-09-03 03:01:01 +0100

    files

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 carousel using HTML,CSS and Javascript
 
-[Link to live site](https://mutiatbash.github.io/Carousel/)
+[Link to live site](https://mutiatbash.github.io/image-gallery/)
--- index.js
+++ index.js
@@ -2,101 +2,102 @@ const slideImage = document.querySelectorAll(".image");
 const slideContainer = document.querySelector(".car-image");
 const nextBtn = document.querySelector(".next-arrow .next");
 const prevBtn = document.querySelector(".prev-arrow");
-const navDots = document.querySelector(".dots");
-const dot = document.querySelectorAll(".dot");
+const modal = document.querySelector(".modal");
+const thumbs = document.querySelector(".thumbs");
+const imgdescription = document.querySelectorAll(".title");
+const thumbnails = document.querySelectorAll(".thumbnail img");
+const closeBtn = document.querySelector(".close");
 
 let noImages = slideImage.length;
 let slideWidth = slideImage[0].clientWidth;
 let currentSlide = 0;
 
-// set up the slider
+//Go to slide
+function goToSlide(slideNumber) {
+  slideContainer.style.transform =
+    "translateX(-" + slideWidth * slideNumber + "px)";
 
-function init() {
-    /*
-    slideImage[0]= 0
-    slideImage[1]= 100%
-    slideImage[2]= 200%
-    */
+  currentSlide = slideNumber;
 
-  slideImage.forEach((img, i) => {
-    img.style.left = i * 100 + "%";
-  });
+  activeClass();
+}
 
-  slideImage[0].classList.add("active")
-  
-};
-
-init();
-
-//create nav dots
-
-// function createNavDots() {
-   //for (i = 0; i < noImages; i++){
-    //const dot = document.createElement("div");
-    //dot.classList.add("dot");
-    //navDots.appendChild(dot);
-   //}
-
-   //navDots.children[0].classList.add("active");
-   //}
- 
-//next btn
+// //next btn
 
 nextBtn.addEventListener("click", () => {
-   if (currentSlide >= noImages - 1) {
-    goToSlide(0);
-    currentSlide = 0;
-    return;
+  if (modal.style.display === "block") {
+    if (currentSlide < noImages - 1) {
+      // goToSlide(0);
+      openModal(currentSlide + 1);
     }
-
-    currentSlide++;
-    //slideContainer.style.transform = "translateX(-" + slideWidth + "px)";
-    goToSlide(currentSlide); 
+  }
 });
 
-//prev btn
+// //prev btn
 
 prevBtn.addEventListener("click", () => {
-    if (currentSlide <= 0) {
-     goToSlide(noImages - 1);
-     currentSlide = noImages - 1;
-     return;
-     }
- 
-     currentSlide--;
-     goToSlide(currentSlide); 
- });
- 
- //Go to slide
- function goToSlide(slideNumber) {
-     slideContainer.style.transform = "translateX(-" + slideWidth * slideNumber + "px)";
+  if (modal.style.display === "block") {
+    if (currentSlide > 0) {
+      // goToSlide(noImages - 1);
+      openModal(currentSlide - 1);
+    }
+  }
+});
 
-     currentSlide = slideNumber;
+//  //set active class
 
-     activeClass();
-
- }
- 
-
- //set active class
-
- function activeClass() {
+function activeClass() {
   // set active class for slide image
 
   let currentActive = document.querySelector(".image.active");
   currentActive.classList.remove("active");
   slideImage[currentSlide].classList.add("active");
+}
 
-  let currentDot = document.querySelector(".dot.active");
-  currentDot.classList.remove("active");
-  navDots.children[currentSlide].classList.add("active");
- } 
+// Function to open the modal
+function openModal(index) {
+  currentSlide = index;
+  modal.style.display = "block";
+  slideImage.forEach((img, i) => {
+    if (i === index) {
+      img.style.display = "block";
+    } else {
+      img.style.display = "none";
+    }
+    // img.style.left = i * 100 + "%";
+  });
 
- //dot events
- function dott(){
-  dot.addEventListener("click", () => {
-    goToSlide(1);
- }); 
- }
+  imgdescription.forEach((des, i) => {
+    if (i === index) {
+      des.style.display = "block";
+    } else {
+      des.style.display = "none";
+    }
+  });
 
- dott();
+  // title.textContent = imgdescription[index].textContent;
+}
+
+// Function to close the modal
+function closeModal() {
+  modal.style.display = "none";
+}
+
+// Event listeners for thumbnail clicks
+thumbnails.forEach((thumb, index) => {
+  thumb.addEventListener("click", () => {
+    openModal(index);
+  });
+});
+
+// Event listener for closing the modal
+closeBtn.addEventListener("click", () => {
+  closeModal();
+});
+
+// Close modal when clicking outside of the modal content
+window.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    closeModal();
+  }
+});
